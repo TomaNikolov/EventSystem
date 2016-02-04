150cb678db0f53224a4f821aec927da2687ed142
TomaNikolov <on.tomas@gmail.com>
2016-02-04 22:01:32 +0200

    added ui theme

--- Source/EventSystem/Web/EventSystem.Web/App_Start/BundleConfig.cs
+++ Source/EventSystem/Web/EventSystem.Web/App_Start/BundleConfig.cs
@@ -7,8 +7,14 @@
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            RegisterScriptBundels(bundles);
+            RegisterStyleBundels(bundles);
+        }
+               
+        private static void RegisterScriptBundels(BundleCollection bundles)
+        {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                       "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -21,6 +27,21 @@
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/theme").Include(
+                    "~/Content/Theme/js/*.js"));
+        }
+
+        private static void RegisterStyleBundels(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/theme").Include(
+                  "~/Content/Theme/css/bootstrap.min.css",
+                    "~/Content/Theme/css/font.awsome.min.css",
+                     "~/Content/Theme/css/main.css",
+                     "~/Content/Theme/css/animate.css",
+                     "~/Content/Theme/css/animate.css",
+                   
+                     "~/Content/Theme/css/responsive.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
