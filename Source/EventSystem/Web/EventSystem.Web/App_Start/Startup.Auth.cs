60148d3b24054d86657d496d8d8a2821641451b0
TomaNikolov <on.tomas@gmail.com>
2016-02-04 16:01:46 +0200

    extracted db context from web project

--- Source/EventSystem/Web/EventSystem.Web/App_Start/Startup.Auth.cs
+++ Source/EventSystem/Web/EventSystem.Web/App_Start/Startup.Auth.cs
@@ -7,16 +7,15 @@
     using Microsoft.Owin.Security.Cookies;
     using Owin;
 
-    using EventSystem.Web.Models;
     using EventSystem.Models;
-
+    using Data;
     public partial class Startup
     {
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext(EventSystemDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
