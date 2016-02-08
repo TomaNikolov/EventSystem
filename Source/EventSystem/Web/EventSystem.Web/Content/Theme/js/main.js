8b45821b9626a587b9ff580aa62a9148d8005642
TomaNikolov <on.tomas@gmail.com>
2016-02-08 15:21:25 +0200

    rename index action to details

--- Source/EventSystem/Web/EventSystem.Web/Content/Theme/js/main.js
+++ Source/EventSystem/Web/EventSystem.Web/Content/Theme/js/main.js
@@ -1,20 +1,8 @@
 jQuery(function($) {'use strict',
-	
-	////Countdown js
-	// $("#countdown").countdown({
-	//		date: "10 july 2017 12:00:00",
-	//		format: "on"
-	//	},
-		
-	//	function() {
-	//		// callback function
-	//	});
-	    	
 	// Carousel Auto Slide Off
 	$('#event-carousel, #twitter-feed, #sponsor-carousel ').carousel({
 		interval: false
 	});
-
 
 	// Contact form validation
 	var form = $('.contact-form');
@@ -26,19 +14,14 @@ jQuery(function($) {'use strict',
 		return false;
 	});
 
-	$( window ).resize(function() {
-		menuToggle();
-	});
-
-	$('.main-nav ul').onePageNav({
-		currentClass: 'active',
-	    changeHash: false,
-	    scrollSpeed: 900,
-	    scrollOffset: 0,
-	    scrollThreshold: 0.3,
-	    filter: ':not(.no-scroll)'
-	});
-
+	//$('.main-nav ul').onePageNav({
+	//	currentClass: 'active',
+	//    changeHash: false,
+	//    scrollSpeed: 900,
+	//    scrollOffset: 0,
+	//    scrollThreshold: 0.3,
+	//    filter: ':not(.no-scroll)'
+	//});
 });
 
 
