0c887601ac876b0430257d45f15c1faea6095730
TomaNikolov <on.tomas@gmail.com>
2016-02-11 16:49:39 +0200

    extracted javascript from view to separate files

---
+++ Source/EventSystem/Web/EventSystem.Web/Scripts/EventMaker/Places/Create/images.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $(input)
+                    .parent()
+                    .parent()
+                    .find('img')
+                    .attr('src', e.target.result)
+                    .show(10);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $(".upload").change(function () {
+        readURL(this);
+    });
+
+    $('.img-responsive').hide();
+}());
---
+++ Source/EventSystem/Web/EventSystem.Web/Scripts/EventMaker/Places/Create/maps.js
@@ -0,0 +1,74 @@
+﻿(function () {
+    //Get Addres longitude and latitude
+    $('#Street').blur(function () {
+        GetAddressGeoLocation();
+    })
+
+    $('form').submit(function (e) {
+        $this = $(this);
+        if (!$('#Latitude').val()) {
+            e.preventDefault();
+
+            GetAddressGeoLocation()
+             .then(function () {
+                 console.log('ok')
+                 $this.unbind("submit").submit()
+             });
+        }
+    })
+
+    function GetAddressGeoLocation() {
+        var address = $('#Street').val();
+        var feedback = $('#feedback');
+        var city = $('#CityId option:selected').text()
+        console.log(city);
+
+        return $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + city)
+                 .then(function (result) {
+                     if (result.status === 'OK') {
+                         var address = result.results[0].formatted_address;
+                         var numCommas = address.match(/,/g).length;
+                         if (numCommas == 2) {
+                             feedback.removeClass();
+                             feedback.addClass('icon-ok');
+                             feedback.css('color', 'green');
+
+                             var location = result.results[0].geometry.location;
+                             var latitude = $('#Latitude');
+                             var longitude = $('#Longitude');
+                             latitude.val(parseFloat(location.lat));
+                             longitude.val(location.lng);
+                             console.log(location.lat);
+                             console.log(location.lng);
+                             AddMarker(location.lat, location.lng)
+                         } else {
+                             feedback.removeClass();
+                             feedback.addClass('icon-remove');
+                             feedback.css('color', 'red');
+                         }
+                     }
+                 })
+    }
+
+    //Google Maps
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 42.1523245, lng: 24.7484875 },
+        scrollwheel: false,
+        zoom: 15
+    });
+
+    var marker = new google.maps.Marker();
+
+    function AddMarker(lat, long, address) {
+        var latLng = { lat: lat, lng: long };
+
+        marker.setMap(null);
+        map.setCenter(latLng)
+
+        marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            title: address
+        });
+    }
+}());
