dd8fba8765ec4dbe3b67046f13ecc3e8a6aff7be
TomaNikolov <on.tomas@gmail.com>
2016-02-14 23:57:36 +0200

    added some constants

---
+++ Source/EventSystem/Web/EventSyslem.Web.Infrastructure/Constants/Partials.cs
@@ -0,0 +1,7 @@
+﻿namespace EventSystem.Web.Infrastructure.Constants
+{
+    public class Partials
+    {
+        public const string _Notification.cshtml = "~/Views/Shared/_Notification.cshtml";
+    }
+}
---
+++ Source/EventSystem/Web/EventSyslem.Web.Infrastructure/Constants/Views.cs
@@ -0,0 +1,15 @@
+﻿namespace EventSystem.Web.Infrastructure.Constants
+{
+    public class Views
+    {
+        public const string Index = "Index";
+
+        public const string All = "All";
+
+        public const string Create = "Create";
+
+        public const string Edit = "Edit";
+
+        public const string Detatils = "Detatils";
+    }
+}
