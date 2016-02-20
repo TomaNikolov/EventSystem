93122ca55263e3a06939022f473f2c6e0b34d982
TomaNikolov <on.tomas@gmail.com>
2016-02-20 23:52:53 +0200

    added places populator

---
+++ Source/EventSystem/Web/EventSyslem.Web.Infrastructure/Populators/PopulatePlacesAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace EventSystem.Web.Infrastructure.Populators
+{
+    using System.Web.Mvc;
+
+    using Data.Common.Repositories;
+    using Models;
+    using Ninject;
+
+    public class PopulatePlacesAttribute : BasePopulator
+    {
+        private const string Places = "Places";
+
+        [Inject]
+        public IDbRepository<Place> PLacesRepository { private get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            filterContext.Controller.ViewBag.Countries = base.GetSelecTedList(this.PLacesRepository, Places);
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
