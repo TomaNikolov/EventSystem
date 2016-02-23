fcda3b3b166b74be437487a7960b844bb4574ad5
TomaNikolov <on.tomas@gmail.com>
2016-02-23 10:50:21 +0200

    implemented order service

--- Source/EventSystem/Data/EventSystem.Models/OrderItem.cs
+++ Source/EventSystem/Data/EventSystem.Models/OrderItem.cs
@@ -1,8 +1,22 @@
 ﻿namespace EventSystem.Models
 {
     using EventSystem.Data.Common.Models;
+    using System.ComponentModel.DataAnnotations;
 
     public class OrderItem : BaseModel<int>
     {
+        public int TicketId { get; set; }
+
+        public virtual Ticket Ticket { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        public int EventId { get; set; }
+
+        public virtual Event Event { get; set; }
     }
 }
---
+++ Source/EventSystem/Services/EventSystem.Services/Contracts/IOrdersService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using EventSystem.Models;
+
+namespace EventSystem.Services.Contracts
+{
+    public interface IOrdersService
+    {
+        bool Create(string userId, int addressId, ICollection<OrderItem> tickets);
+    }
+}
